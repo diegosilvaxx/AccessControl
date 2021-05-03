79ae25d557a3630472cdf3a7f8b3dc2d17e4aee5
Diego Borges <dborges@exactsales.com.br>
2021-05-03 12:10:32 -0300

    first commit

---
+++ Access Control/Server/Data/Migrations/20210503132836_createBanco.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Access_Control.Server.Data.Migrations
+{
+    public partial class createBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
